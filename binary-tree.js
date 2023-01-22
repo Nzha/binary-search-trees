730634bc24bfe4c1c3f9b68cdd121e9d3b0d6dc7
Adrien <adrien.biz@gmail.com>
2023-01-22 20:53:25 +0400

    Sort array and remove duplicates

--- binary-tree.js
+++ binary-tree.js
@@ -31,8 +31,18 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
   }
 };
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const formatArray = (arr) => {
+  const sortedArr = arr.sort((a, b) => a - b);
+  const removeDupArr = sortedArr.filter((el, index) => {
+    return sortedArr.indexOf(el) === index;
+  });
+  console.log(removeDupArr);
+  return removeDupArr;
+}
 
-let test = buildTree(arr, 0, 8);
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
-prettyPrint(test);
+let tree = buildTree(formatArray(arr), 0, 8);
+
+prettyPrint(tree);
